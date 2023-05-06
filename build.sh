be37e9d42fbae582b321e696b0ad177af0a60841
root <root@localhost.localdomain>
2023-05-06 15:14:18 +0800

    rename

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 RUN_NAME="lunaticvibes-gateway"
+git submodule update --remote --merge && sh gen_proto.sh
 rm -rf  go.sum && go mod tidy && go mod vendor
 rm -rf output
 mkdir output
-git submodule update --remote --merge && sh gen_proto.sh
 GOOS=linux GOARCH=amd64 go build -gcflags="all=-N -l" -o output/${RUN_NAME}
 chmod +x output/${RUN_NAME}
 
