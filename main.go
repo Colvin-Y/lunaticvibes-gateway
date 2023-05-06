7becb81a1d3a40112a2a8f8cae7f650f08b6b2bc
root <root@localhost.localdomain>
2023-05-06 15:29:01 +0800

    hello world gateway

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 import (
 	"context"
 	"log"
-	"net"
 	"net/http"
 	helloworldpb "github.com/Colvin-Y/lunaticvibes-gateway/proto/helloworld"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime" // 注意v2版本
@@ -10,37 +9,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-type server struct {
-	helloworldpb.UnimplementedGreeterServer
-}
-
-func NewServer() *server {
-	return &server{}
-}
-
-func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
-	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
-}
-
 func main() {
-	// Create a listener on TCP port
-	lis, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatalln("Failed to listen:", err)
-	}
-
-	// 创建一个gRPC server对象
-	s := grpc.NewServer()
-	// 注册Greeter service到server
-	helloworldpb.RegisterGreeterServer(s, &server{})
-	// 8080端口启动gRPC Server
-	log.Println("Serving gRPC on 0.0.0.0:8080")
-	go func() {
-		log.Fatalln(s.Serve(lis))
-	}()
-
-	// 创建一个连接到我们刚刚启动的 gRPC 服务器的客户端连接
-	// gRPC-Gateway 就是通过它来代理请求（将HTTP请求转为RPC请求）
 	conn, err := grpc.DialContext(
 		context.Background(),
 		"0.0.0.0:8080",
